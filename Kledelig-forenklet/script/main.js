327482a473a5768bac142108fe6bce9ebe032278
h170919 <52938064+h170919@users.noreply.github.com>
2023-10-22 13:12:59 +0200

    Fjernet plassholder navn navnesen og erstattet med registrert navn

--- Kledelig-forenklet/script/main.js
+++ Kledelig-forenklet/script/main.js
@@ -1,22 +1,26 @@
 function kontoOppretting(skjemaData){
-	try{
-		// Henter inn alle input-felt
-		let inputs = skjemaData.querySelectorAll('input');
-		
-		// Klargjør objekt som skal inneholde innsendte data
-		let tilLagring = {};
-		
-		// legger inn hvert felt og dets verdi
-		inputs.forEach((inp) => {
-			tilLagring[inp.id] = inp.value;
-		});
-		
-		// Lagrer i browserens database
-		localStorage.setItem('userAccountData', JSON.stringify(tilLagring));
-		
-		return '<h3>Takk ' + skjemaData.firstName.value + ', du er nå registrert hos Kledeli!</h3>';
+    try{
+        // Henter inn alle input-felt
+        let inputs = skjemaData.querySelectorAll('input');
+        
+        // Klargjør objekt som skal inneholde innsendte data
+        let tilLagring = {};
+        
+        // legger inn hvert felt og dets verdi
+        inputs.forEach((inp) => {
+            tilLagring[inp.id] = inp.value;
+        });
 
-	}catch(e){
-		return '<p class="error">Feil oppstod under lagring av registreringsdatas: ' + e + '</p>';
-	}
+        // Oppdaterer display navn og email
+        document.getElementById('displayName').innerHTML = `<b>Navn:</b> ${tilLagring['firstName']} ${tilLagring['lastName']}`;
+        document.getElementById('displayEmail').innerHTML = `<b>E-post:</b> ${tilLagring['email']}`;
+        
+        // Lagrer i browserens database
+        localStorage.setItem('userAccountData', JSON.stringify(tilLagring));
+        
+        return '<h3>Takk ' + tilLagring['firstName'] + ', du er nå registrert hos Kledeli!</h3>';
+
+    }catch(e){
+        return '<p class="error">Feil oppstod under lagring av registreringsdatas: ' + e + '</p>';
+    }
 }
